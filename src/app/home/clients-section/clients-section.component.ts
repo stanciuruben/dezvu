01d154cf852695afc72537377c31514407266453
Ruben Stanciu <stanciu.ruben20@gmail.com>
2023-06-30 16:20:25 +0200

    fix: wrong paths

--- src/app/home/clients-section/clients-section.component.ts
+++ src/app/home/clients-section/clients-section.component.ts
@@ -37,31 +37,31 @@ export class ClientsSectionComponent {
     {
       name: 'Zeplin',
       alt: 'Illstration of a yellow zeppelin | Zeplin logo',
-      icon: '/assets/images/zeplin-icon.png',
+      icon: 'assets/images/zeplin-icon.png',
       bgColor: '#fbf7f0',
     },
     {
       name: 'Dropbox',
       alt: 'Illstration of a minimalist blue open box | Dropbox logo',
-      icon: '/assets/images/dropbox-icon.png',
+      icon: 'assets/images/dropbox-icon.png',
       bgColor: '#f2f6fc',
     },
     {
       name: 'Shopify',
       alt: 'Illstration of a green bag with the initial S on it | Shopify logo',
-      icon: '/assets/images/shopify-icon.png',
+      icon: 'assets/images/shopify-icon.png',
       bgColor: '#f1ffd6',
     },
     {
       name: 'Slack',
       alt: 'Slack logo',
-      icon: '/assets/images/slack-icon.png',
+      icon: 'assets/images/slack-icon.png',
       bgColor: '#f5f5f5',
     },
     {
       name: 'WooCommerce',
       alt: 'Purple box with white text that says "Woo" | WooCommerce logo',
-      icon: '/assets/images/woocommerce-icon.png',
+      icon: 'assets/images/woocommerce-icon.png',
       bgColor: '#fff0fc',
     },
   ];
